dd134431b507e1e2046c17636cb3424fd70134eb
tomatoaiu <boushi_sukaimu@yahoo.co.jp>
2020-10-12 21:13:52 +0900

    delete *.js and update tsconfig.json

--- src/index.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-test('炎なら草、氷、水、雷', () => {
-    expect(index_1.combatSystem([0 /* Fire */])).toEqual(expect.arrayContaining([4 /* Straw */, 5 /* Ice */, 1 /* Water */, 3 /* Thunder */]));
-});
